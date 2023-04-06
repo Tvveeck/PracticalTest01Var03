0ad6242fdc46bafb4f83671792d290d3511d959b
Tweeck <alexmotoi@yahoo.com>
2023-04-06 08:28:59 +0300

    Exercitiul A

--- app/src/main/java/ro/pub/cs/systems/eim/PracticalTest01Var03/PracticalTest01Var03MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/PracticalTest01Var03/PracticalTest01Var03MainActivity.java
@@ -3,12 +3,28 @@ package ro.pub.cs.systems.eim.PracticalTest01Var03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PracticalTest01Var03MainActivity extends AppCompatActivity {
 
+    Button plusButton;
+    Button minusButton;
+    Button navigateButton;
+
+    EditText number1;
+    EditText number2;
+
+    TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actiivity_practical_test01_var03_main);
+
+        plusButton = findViewById(R.id.plus);
+        minusButton = findViewById(R.id.minus);
+        navigateButton = findViewById(R.id.navigate);
+
     }
 }
