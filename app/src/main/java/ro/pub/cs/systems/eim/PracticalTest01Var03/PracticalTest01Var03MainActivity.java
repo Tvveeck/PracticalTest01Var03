9a2ac366aea0bb471cde6b40e7f8d1e5f41ab0a2
Tweeck <alexmotoi@yahoo.com>
2023-04-06 08:13:06 +0300

    Initial commit

---
+++ app/src/main/java/ro/pub/cs/systems/eim/PracticalTest01Var03/PracticalTest01Var03MainActivity.java
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.PracticalTest01Var03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PracticalTest01Var03MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actiivity_practical_test01_var03_main);
+    }
+}
