4e54c30cc80709a07ff3eb5eb37d6cb4ef37a8a8
Tweeck <alexmotoi@yahoo.com>
2023-04-06 08:39:05 +0300

    Exercitiul B

--- app/src/main/java/ro/pub/cs/systems/eim/PracticalTest01Var03/PracticalTest01Var03MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/PracticalTest01Var03/PracticalTest01Var03MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PracticalTest01Var03MainActivity extends AppCompatActivity {
 
@@ -26,5 +27,71 @@ public class PracticalTest01Var03MainActivity extends AppCompatActivity {
         minusButton = findViewById(R.id.minus);
         navigateButton = findViewById(R.id.navigate);
 
+        number1 = findViewById(R.id.number1);
+        number2 = findViewById(R.id.number2);
+
+        result = findViewById(R.id.result);
+
+        plusButton.setOnClickListener(it -> {
+
+            try {
+                Integer.parseInt(number1.getText().toString());
+                Integer.parseInt(number2.getText().toString());
+            } catch (NumberFormatException e) {
+                Toast.makeText(this, "Input must be a number", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            Integer left = Integer.parseInt(number1.getText().toString());
+            Integer right = Integer.parseInt(number2.getText().toString());
+            result.setText(number1.getText().toString() + " + " + number2.getText().toString() + " = " + String.valueOf(left + right));
+        });
+
+        minusButton.setOnClickListener(it -> {
+
+            try {
+                Integer.parseInt(number1.getText().toString());
+                Integer.parseInt(number2.getText().toString());
+            } catch (NumberFormatException e) {
+                Toast.makeText(this, "Input must be a number", Toast.LENGTH_LONG).show();
+                return;
+            }
+
+            Integer left = Integer.parseInt(number1.getText().toString());
+            Integer right = Integer.parseInt(number2.getText().toString());
+            result.setText(number1.getText().toString() + " - " + number2.getText().toString() + " = " + String.valueOf(left - right));
+        });
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString("result", result.getText().toString());
+        savedInstanceState.putString("number1", number1.getText().toString());
+        savedInstanceState.putString("number2", number2.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        String got = "Saved values: ";
+
+        if (savedInstanceState.containsKey("result")) {
+            got += savedInstanceState.getString("result") + " | ";
+            result.setText(savedInstanceState.getString("result"));
+        }
+        if (savedInstanceState.containsKey("number1")) {
+            got += savedInstanceState.getString("number1") + " | ";
+            number1.setText(savedInstanceState.getString("number1"));
+        }
+        if (savedInstanceState.containsKey("number2")) {
+            got += savedInstanceState.getString("number2") + " | ";
+            number2.setText(savedInstanceState.getString("number2"));
+        }
+
+        Toast.makeText(this, got, Toast.LENGTH_LONG).show();
+    }
+
 }
+
